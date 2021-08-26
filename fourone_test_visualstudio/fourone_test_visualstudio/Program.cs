acf3016624fa6e0c516963d97f3c60b8fb335c79
zokjebi <myimgman@gmail.com>
2021-08-26 11:23:45 +0900

    Update Program.cs

--- fourone_test_visualstudio/fourone_test_visualstudio/Program.cs
+++ fourone_test_visualstudio/fourone_test_visualstudio/Program.cs
@@ -7,6 +7,8 @@ namespace fourone_test_visualstudio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Hello World!_2");
         }
     }
 }
